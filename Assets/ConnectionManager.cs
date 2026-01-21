36f644939853f5175927739f2c8940837a19d72d
malken21 <77374813+malken21@users.noreply.github.com>
2026-01-21 18:35:29 +0900

    Update ConnectionManager.cs

--- Assets/ConnectionManager.cs
+++ Assets/ConnectionManager.cs
@@ -2,10 +2,40 @@ using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
-public class ConnectionManager : MonoBehaviour
+using Unity.Collections;
+
+public class ConnectionManager : NetworkBehaviour
 {
+    public static ConnectionManager Instance { get; private set; }
+
     [Header("Development Settings")]
+    [SerializeField] private string initialServerUrl = "http://localhost:3000";
     public bool autoStartClientInEditor = true;
+
+    private NetworkVariable<FixedString512Bytes> _serverUrl = new NetworkVariable<FixedString512Bytes>();
+
+    public string serverUrl => _serverUrl.Value.IsEmpty ? initialServerUrl : _serverUrl.Value.ToString();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            _serverUrl.Value = new FixedString512Bytes(initialServerUrl);
+        }
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     void Start()
     {
